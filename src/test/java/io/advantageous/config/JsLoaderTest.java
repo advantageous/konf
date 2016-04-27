872cd1492608b14168c3367f67c92c8fcde42fff
Geoffrey Chandler <geoffc@gmail.com>
2016-04-26 20:09:47 -0700

    enforce utility class is never instantiated

--- src/test/java/io/advantageous/config/JsLoaderTest.java
+++ src/test/java/io/advantageous/config/JsLoaderTest.java
@@ -3,10 +3,23 @@ package io.advantageous.config;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.net.URI;
 import java.util.Map;
 
 public class JsLoaderTest {
+
+    @Test(expected = InvocationTargetException.class)
+    public void testPrivateConstructor() throws Exception {
+        Assert.assertEquals(0, JsLoader.class.getConstructors().length);
+        Assert.assertEquals(1, JsLoader.class.getDeclaredConstructors().length);
+        Constructor constructor = JsLoader.class.getDeclaredConstructor();
+        Assert.assertNotNull(constructor);
+        Assert.assertFalse(constructor.isAccessible());
+        constructor.setAccessible(true);
+        constructor.newInstance();
+    }
 
     @Test
     public void testLoadConfig() throws Exception {
