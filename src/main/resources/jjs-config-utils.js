2d89e2c3dee7951554c93b15bccad411905c3000
RichardHightower <richard.m.hightower@gmail.com>
2016-04-29 11:32:09 -0700

    check in os

--- src/main/resources/jjs-config-utils.js
+++ src/main/resources/jjs-config-utils.js
@@ -1,13 +1,17 @@
 var env = Java.type("java.lang.System").getenv;
 var uri = Java.type("java.net.URI").create;
-
+var system = Java.type("java.lang.System");
 var duration = Java.type("java.time.Duration");
+
+/** To store private vars. */
+var konf = {
+  osNameInternal : system.getProperty("os.name").toLowerCase()
+};
 
 function dockerHostOrDefault(defaultHost) {
   var dockerHost = env("DOCKER_HOST");
   return dockerHost ? uri(dockerHost).getHost() : defaultHost;
 }
-
 
 function seconds(unit) {
   return duration.ofSeconds(unit);
@@ -35,4 +39,26 @@ function milliseconds(unit) {
   return duration.ofMillis(unit);
 }
 
+function sysProp(prop) {
+  return system.getProperty(prop);
+}
 
+function sysPropOrDefault(prop, defaultValue) {
+  return system.getProperty(prop, defaultValue.toString());
+}
+
+function isWindowsOS() {
+  return (konf.osNameInternal.indexOf("win") >= 0);
+}
+
+function isMacOS() {
+  return (konf.osNameInternal.indexOf("mac") >= 0);
+}
+
+function isUnix() {
+  return (konf.osNameInternal.indexOf("nix") >= 0 || OS.indexOf("nux") >= 0 || OS.indexOf("aix") > 0 );
+}
+
+function  isSolaris() {
+  return (konf.osNameInternal.indexOf("sunos") >= 0);
+}
